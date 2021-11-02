d39068dd212f4efeac288c548a960f42a63dc4d2
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-11-02 23:00:54 +0200

    create BarnItemDiffCallback and refactor BarnListAdapter with ListAdapter

--- app/src/main/java/com/example/barnbook/presentation/BarnListAdapter.kt
+++ app/src/main/java/com/example/barnbook/presentation/BarnListAdapter.kt
@@ -1,15 +1,12 @@
 package com.example.barnbook.presentation
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.barnbook.R
 import com.example.barnbook.domain.BarnItem
 
-class BarnListAdapter : ListAdapter<BarnItem, BarnListAdapter.BarnItemViewHolder>(BarnItemDiffCallback()){
+class BarnListAdapter : ListAdapter<BarnItem, BarnItemViewHolder>(BarnItemDiffCallback()){
 
 
 var onBarnItemLongClickListener:((BarnItem) -> Unit)? =null
@@ -48,13 +45,6 @@ true
 
 
 
-    }
-
-
-
-    class BarnItemViewHolder( val view: View):RecyclerView.ViewHolder(view){
-        val tvName: TextView =view.findViewById(R.id.tv_name)
-        val tvCount: TextView =view.findViewById(R.id.tv_count)
     }
 companion object{
     const val VIEW_TYPE_DISABLED = -1
