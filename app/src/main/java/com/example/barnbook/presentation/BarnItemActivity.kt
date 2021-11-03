858534d04da7fa365394f1c7923e73492e1e40fa
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-11-03 12:29:30 +0200

    create second activity

---
+++ app/src/main/java/com/example/barnbook/presentation/BarnItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.barnbook.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.barnbook.R
+
+class BarnItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barn_item)
+    }
+}
