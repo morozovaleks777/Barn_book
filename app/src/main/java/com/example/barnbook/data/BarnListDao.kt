00015c7334c119afa509d884eda2e2115e1c1196
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-11-26 16:13:58 +0200

    create DB

---
+++ app/src/main/java/com/example/barnbook/data/BarnListDao.kt
@@ -0,0 +1,24 @@
+package com.example.barnbook.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.barnbook.domain.BarnItem
+
+@Dao
+interface BarnListDao {
+
+    @Query("SELECT * FROM barn_items")
+fun getBarnItem():LiveData<List<BarnItemDBModel>>
+
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+fun addBarnItem(barnItemDBModel: BarnItemDBModel)
+
+@Query("DELETE FROM barn_items WHERE itemId=:barnItemId")
+fun deleteBarnItem(barnItemId:Int)
+
+@Query("SELECT * FROM barn_items WHERE itemId=:barnItemId LIMIT 1")
+fun getBarnItem(barnItemId:Int):BarnItemDBModel
+}
