845d529d795d22b1e219d8b0480a4c8f4d4e9c4b
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-11-27 23:20:34 +0200

    Transformation

--- app/src/main/java/com/example/barnbook/data/BarnListRepositoryImpl.kt
+++ app/src/main/java/com/example/barnbook/data/BarnListRepositoryImpl.kt
@@ -2,28 +2,14 @@ package com.example.barnbook.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.barnbook.domain.BarnItem
 import com.example.barnbook.domain.BarnListRepository
-import java.lang.RuntimeException
-import kotlin.random.Random
 
 class BarnListRepositoryImpl(application: Application):BarnListRepository {
-//    private val barnList = sortedSetOf<BarnItem>({ o1, o2 -> o1.itemId.compareTo(o2.itemId) })
-//    private val barnListLD=MutableLiveData<List<BarnItem>>()
-//    private var autoIncrementId=0
 
 private val barnListDao=AppDatabase.getInstance(application).barnListDao()
     private val mapper=BarnListMapper()
-
-//init {
-//    for (i in 0 until 20){
-//        val item=BarnItem(" name ",0, 0.0F ,false)
-//        addBarnItem(item)
-//    }
-//}
 
     override fun deleteBarnItem(barnItem: BarnItem) {
       barnListDao.deleteBarnItem(barnItem.itemId)
@@ -37,7 +23,7 @@ private val barnListDao=AppDatabase.getInstance(application).barnListDao()
     override fun getAmount(list: List<BarnItem>): Float {
         var sum=0F
 
-        for (i in 0 until list.size-1){
+        for (i in list.indices){
            sum+= list[i].count*list[i].price
 
         }
